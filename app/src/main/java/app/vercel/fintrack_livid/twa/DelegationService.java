a722dde3690a8c7d8d0c77efe6bfe7c34f93d352
vaibhav-khapra <vaibhavkhapra5@gmail.com>
2025-11-09 21:44:13 +0530

    Created App

---
+++ app/src/main/java/app/vercel/fintrack_livid/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.fintrack_livid.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
